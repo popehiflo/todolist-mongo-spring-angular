73661c6ca8533ef34b0fcb87e5c28b9821ea50d0
popehiflo <poolpetterhijuel@hotmail.com>
2021-10-06 17:18:22 -0500

    CRUD funcional

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -9,14 +9,56 @@ import { TareaService } from './services/tarea.service';
 export class AppComponent implements OnInit {
 
   tareas: any[] = [];
+  tarea = {
+    id: null,
+    nombre: '',
+    realizada: false
+  }
 
   constructor(private tareaService:TareaService) {
 
   }
 
   ngOnInit(): void {
-    this.tareaService.readAll()
-      .subscribe((data:any) => this.tareas = data);
+    this.readAll();
+  }
+
+
+
+  createOrUpdate() {
+    if (this.tarea.id) {
+      this.tareaService.update(this.tarea.id!, this.tarea).subscribe(
+        () => this.readAll()
+      );
+    } else {
+      this.tareaService.create(this.tarea).subscribe(
+        () => this.readAll()
+      );
+    }
+
+    this.tarea = {
+      id: null,
+      nombre: '',
+      realizada: false
+    };
+  }
+
+  readAll() {
+    this.tareaService.readAll().subscribe(
+      (data:any) => this.tareas = data
+    );
+  }
+
+  edit(tareaRecibida:any) {
+    this.tarea = {
+      ...tareaRecibida
+    };
+  }
+
+  delete(tareaRecibida:any) {
+    this.tareaService.delete(tareaRecibida.id).subscribe(
+      () => this.readAll()
+    );
   }
 
 }
