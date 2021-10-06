2d2d0bc0f6264bca1d0ad9160ff3889433e9cac7
popehiflo <poolpetterhijuel@hotmail.com>
2021-10-06 16:16:04 -0500

    listar tareas

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TareaService } from './services/tarea.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+
+  tareas: any[] = [];
+
+  constructor(private tareaService:TareaService) {
+
+  }
+
+  ngOnInit(): void {
+    this.tareaService.readAll()
+      .subscribe((data:any) => this.tareas = data);
+  }
+
 }
