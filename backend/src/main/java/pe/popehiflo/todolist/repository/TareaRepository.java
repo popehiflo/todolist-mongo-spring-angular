75abf6513162f9d557909087c2298f7f97015368
popehiflo <poolpetterhijuel@hotmail.com>
2021-10-06 11:32:54 -0500

    added repository Tarea

---
+++ backend/src/main/java/pe/popehiflo/todolist/repository/TareaRepository.java
@@ -0,0 +1,9 @@
+package pe.popehiflo.todolist.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import pe.popehiflo.todolist.model.Tarea;
+
+@Repository
+public interface TareaRepository  extends MongoRepository<Tarea, Integer> {
+}
